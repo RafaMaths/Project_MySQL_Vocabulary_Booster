9966261626b2a3bd63c6cfc5685c36dbbb339ae0
RafaMaths <rmathscoach@gmail.com>
2021-07-28 15:04:43 -0300

    CHALLENGE 3

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,1 @@
--- Exiba os cargos com a diferença entre seus salários máximo e mínimo
-SELECT
-  JOB_TITLE AS 'Cargo',
-  (MAX_SALARY - MIN_SALARY) AS 'Diferença entre salários máximo e mínimo'
-FROM jobs;
+
