7a05875d45f010929c51817e933e6493c475505c
RafaMaths <rmathscoach@gmail.com>
2021-07-28 17:37:42 -0300

    CHALLENGE 5 FIX

--- desafio5.sql
+++ desafio5.sql
@@ -4,7 +4,8 @@ SELECT
   ROUND(AVG(MIN_SALARY / 12), 2) AS 'Média mínima mensal',
   ROUND(AVG(MAX_SALARY / 12), 2) AS 'Média máxima mensal'
 FROM jobs
-GROUP BY MAX_SALARY,
-         MIN_SALARY,
-         JOB_TITLE
+GROUP BY 
+          MAX_SALARY,
+          MIN_SALARY,
+          JOB_TITLE
 ORDER BY (MAX_SALARY - MIN_SALARY), job_title;
