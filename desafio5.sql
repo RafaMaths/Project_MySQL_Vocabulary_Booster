0e9475d312bbb3120d98c6ec8852b5f158638b73
RafaMaths <rmathscoach@gmail.com>
2021-07-28 17:34:33 -0300

    CHALLENGE 5 FIX

--- desafio5.sql
+++ desafio5.sql
@@ -1,8 +1,10 @@
 SELECT
   job_title AS 'Cargo',
   (MAX_SALARY - MIN_SALARY) AS 'Variação Salarial',
-  ROUND(AVG(MIN_SALARY) / 12) AS 'Média mínima mensal',
-  ROUND(AVG(MAX_SALARY) / 12) AS 'Média máxima mensal'
+  ROUND(AVG(MIN_SALARY / 12), 2) AS 'Média mínima mensal',
+  ROUND(AVG(MAX_SALARY / 12), 2) AS 'Média máxima mensal'
 FROM jobs
-GROUP BY JOB_TITLE
+GROUP BY MAX_SALARY,
+         MIN_SALARY,
+         JOB_TITLE
 ORDER BY (MAX_SALARY - MIN_SALARY), job_title;
