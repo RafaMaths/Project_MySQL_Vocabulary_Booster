bc84bbae18997f5d357ac460eabb84e609fd5cc7
RafaMaths <rmathscoach@gmail.com>
2021-07-28 15:16:54 -0300

    CHALLENGE 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,1 +1,5 @@
-
+-- Exiba os países e indicando se cada um deles se encontra ou não na região formada pela Europa
+SELECT
+  country_name AS 'País', IF(region_id = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+FROM countries
+ORDER BY country_name;
