313505bb6612d30d5bc7468a9560789409928b66
RafaMaths <rmathscoach@gmail.com>
2021-07-28 17:55:22 -0300

    CHALLENGE 7

---
+++ desafio7.sql
@@ -0,0 +1,9 @@
+SELECT
+  UCASE(CONCAT(first_name, ' ', last_name)) AS 'Nome completo',
+  start_date AS 'Data de início',
+  salary
+FROM employees e
+INNER JOIN job_history jh
+  ON e.EMPLOYEE_ID = jh.EMPLOYEE_ID
+WHERE MONTH(start_date) BETWEEN 1 AND 3
+ORDER BY CONCAT(first_name, ' ', last_name), start_date;
