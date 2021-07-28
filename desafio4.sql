d3ebc8f0e8442a3d6a17e982f26be5ecd5807d10
RafaMaths <rmathscoach@gmail.com>
2021-07-28 16:14:24 -0300

    CHALLENGE 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,10 +1,15 @@
--- Exiba a média salarial e o nível de senioridade de todas as pessoas empregadas, agrupadas pelo cargo
-
-SELECT job_title AS 'Cargo', (MAX_SALARY - MIN_SALARY) AS 'Média salarial' FROM jobs, 
-CASE
-  WHEN MAX_SALARY BETWEEN 5000 AND 10000 THEN 'Baixo'
-  WHEN MAX_SALARY BETWEEN 10001 AND 20000 THEN 'Médio'
-  WHEN MAX_SALARY BETWEEN 20001 AND 30000 THEN 'Alto'
-  ELSE 'Altíssimo'
-END
-AS 'Senioridade';
+SELECT
+  j.job_title AS 'Cargo',
+  AVG(e.salary) AS 'Média salarial',
+  CASE
+    WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
+    WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
+    WHEN ROUND(AVG(e.salary), 2) BETWEEN 7501 AND 10500 THEN 'Sênior'
+    ELSE 'CEO'
+  END
+  AS 'Senioridade'
+FROM jobs j
+INNER JOIN employees e
+  ON j.job_id = e.job_id
+GROUP BY j.job_title
+ORDER BY AVG(e.salary), j.job_title;
