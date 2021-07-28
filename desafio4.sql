5643aa9e608b43c3bfde5a034bfd924a1c864442
RafaMaths <rmathscoach@gmail.com>
2021-07-28 16:33:46 -0300

    CHALLENGE 4 FIX

--- desafio4.sql
+++ desafio4.sql
@@ -1,6 +1,6 @@
 SELECT
   j.job_title AS 'Cargo',
-  AVG(e.salary) AS 'Média salarial',
+  ROUND(AVG(e.salary)) AS 'Média salarial',
   CASE
     WHEN ROUND(AVG(e.salary), 2) BETWEEN 2000 AND 5800 THEN 'Júnior'
     WHEN ROUND(AVG(e.salary), 2) BETWEEN 5801 AND 7500 THEN 'Pleno'
