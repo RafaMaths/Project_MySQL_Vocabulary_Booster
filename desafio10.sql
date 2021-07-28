28e371dc83e6420014318dddc049b523dcf0de23
RafaMaths <rmathscoach@gmail.com>
2021-07-28 18:51:32 -0300

    CHALLENGE 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT
+  ProductName AS 'Produto',
+  MIN(Quantity) AS 'Mínima',
+  MAX(Quantity) AS 'Máxima',
+  ROUND(AVG(Quantity), 2) AS 'Média'
+FROM products p
+INNER JOIN order_details od
+  ON p.ProductID = od.ProductID
+GROUP BY ProductName
+HAVING AVG(Quantity) > 20.00
+ORDER BY AVG(quantity), ProductName
