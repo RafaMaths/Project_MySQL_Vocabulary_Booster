da5daaa478bd366ca4da2f103b42029d0776ece6
RafaMaths <rmathscoach@gmail.com>
2021-07-28 20:36:01 -0300

    CHALLENGE 13

--- desafio11.sql
+++ desafio11.sql
@@ -2,10 +2,11 @@ SELECT
   a.ContactName AS 'Nome',
   a.Country AS 'País',
   COUNT(b.Country) - 1 AS 'Número de compatriotas'
-FROM customers AS a,
-     customers AS b
+FROM  customers AS a,
+      customers AS b
 WHERE a.Country = b.Country
-GROUP BY a.ContactName,
-         a.Country
+GROUP BY 
+          a.ContactName,
+          a.Country
 ORDER BY a.ContactName;
       
