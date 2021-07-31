0caa44f6c232597f772b431288e270b3a2d0856c
RafaMaths <rmathscoach@gmail.com>
2021-07-30 21:59:12 -0300

    CHALLENGE 15

---
+++ desafio15.sql
@@ -0,0 +1,13 @@
+DELIMITER $$
+
+CREATE PROCEDURE buscar_media_por_cargo(
+  IN cargo VARCHAR(50),
+  OUT media_salario DECIMAL(,2)
+)
+BEGIN  
+  SELECT AVG(e.salary) AS 'Média salarial' 
+  FROM employees e
+  INNER JOIN jobs j
+  ON e.job_id = j.job_id
+  WHERE j.JOB_TITLE = cargo
+  INTO media_salario;
