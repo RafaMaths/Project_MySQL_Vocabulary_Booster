7ecfa63b8d2e01cf11e11391f467d2e181d00bb4
RafaMaths <rmathscoach@gmail.com>
2021-07-31 10:42:58 -0300

    CHALLENGE 16

---
+++ desafio16.sql
@@ -0,0 +1,16 @@
+DELIMITER $$
+
+CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario(selectedEmail VARCHAR(25))
+RETURNS INT READS SQL DATA
+BEGIN
+	DECLARE jobCount INT;
+	SELECT COUNT(jh.JOB_ID) 
+	FROM job_history jh
+	INNER JOIN employees e
+	ON jh.employee_id = e.employee_id
+	WHERE email = selectedEmail
+	INTO jobCount;
+	RETURN jobCount;
+END $$
+DELIMITER ;
+SELECT buscar_quantidade_de_empregos_por_funcionario('NKOCHHAR') INTO @AVG_Job;
