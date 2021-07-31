de36fb7449f5f6a1e8ac8febec7bc828f6e2a981
RafaMaths <rmathscoach@gmail.com>
2021-07-30 22:34:02 -0300

    CHALLENGE 15 FIX

--- desafio15.sql
+++ desafio15.sql
@@ -1,15 +1,12 @@
 DELIMITER $$
 
-CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50))
- 
+CREATE PROCEDURE buscar_media_por_cargo(IN cargo VARCHAR(50)) 
 BEGIN  
   SELECT round(AVG(e.salary),2) AS 'Média salarial' 
   FROM employees e
   INNER JOIN jobs j
   ON e.job_id = j.job_id
-  WHERE cargo = j.JOB_TITLE;
-  
+  WHERE cargo = j.JOB_TITLE;  
 END $$
 DELIMITER ;
-
 CALL buscar_media_por_cargo('Programmer');
--- desafio17.sql
+++
@@ -1,5 +0,0 @@
-SELECT round(AVG(e.salary),2) AS 'Média salarial' 
-  FROM employees e
-  INNER JOIN jobs j
-  ON e.job_id = j.job_id
-  WHERE j.JOB_TITLE = cargo;
